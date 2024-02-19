463602abad4b409e51e392ca6cd82803fa893bbe
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2024-02-18 21:07:24 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xadrez_console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
